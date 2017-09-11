fa18989d9a805ad003b5f3cb3996215244d55d69
神乐 <499501029@qq.com>
2017-09-11 14:31:11 +0800

    sum
    sum

---
+++ myjs.js
@@ -0,0 +1,12 @@
+function email()
+{
+    with(emal)
+    {
+        apos=value.indexOf(@);
+        dotpos=value.lastIndexOf(".");
+        if(apos<1||dotpos-apos<2)
+        {
+            
+            span1.innerText="请输入正确格式";
+        }
+}
